acece5bee202a84b400cf33093a49d5b359d90aa
codingcele <marcocelegato9@gmail.com>
2023-01-10 23:11:18 +0100

    finito

--- index.php
+++ index.php
@@ -9,7 +9,76 @@
 </head>
 <body>
     <?php
+        $hotels = [
 
+            [
+                'name' => 'Hotel Belvedere',
+                'description' => 'Hotel Belvedere Descrizione',
+                'parking' => true,
+                'vote' => 4,
+                'distance_to_center' => 10.4
+            ],
+            [
+                'name' => 'Hotel Futuro',
+                'description' => 'Hotel Futuro Descrizione',
+                'parking' => true,
+                'vote' => 2,
+                'distance_to_center' => 2
+            ],
+            [
+                'name' => 'Hotel Rivamare',
+                'description' => 'Hotel Rivamare Descrizione',
+                'parking' => false,
+                'vote' => 1,
+                'distance_to_center' => 1
+            ],
+            [
+                'name' => 'Hotel Bellavista',
+                'description' => 'Hotel Bellavista Descrizione',
+                'parking' => false,
+                'vote' => 5,
+                'distance_to_center' => 5.5
+            ],
+            [
+                'name' => 'Hotel Milano',
+                'description' => 'Hotel Milano Descrizione',
+                'parking' => true,
+                'vote' => 2,
+                'distance_to_center' => 50
+            ],
+        
+        ];
+
+        echo "<table class='table'>
+            <thead>
+                <tr>
+                    <th scope='col'>Name</th>
+                    <th scope='col'>Description</th>
+                    <th scope='col'>Parking</th>
+                    <th scope='col'>Vote</th>
+                    <th scope='col'>Distance to center</th>
+                </tr>
+            </thead>
+            <tbody>";
+
+            foreach($hotels as $hotel) {
+                $name = $hotel["name"];
+                $description = $hotel["description"];
+                if ($hotel["parking"] == "true") {
+                    $parking = "SI";
+                }
+                else {
+                    $parking = "NO";
+                }
+                $vote = $hotel["vote"];
+                $distance = $hotel["distance_to_center"];
+    
+                echo "<tr>" . "<td>" . $name . "</td>" . "<td>" . $description . "</td>" . "<td>" . $parking . "</td>" . "<td>" . $vote . "</td>" . "<td>" . $distance . "</td>" . "</tr>";
+            }
+
+        echo "</tbody>
+            </table>";
+        
     ?>
 </body>
 </html>
